fff0c7e2695bd9ab7c26b715c9d66ac073f695cf
amazefan <32010595+amazefan@users.noreply.github.com>
2018-05-15 23:28:50 +0800

    add something
    add something

---
+++ pyspark_getdata.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 14 21:15:40 2018
+
+@author: Administrator
+"""
+
+import pyspark
+from pyspark import SparkContext
+
+path_voice = r'E:\python.file\structure data\JDATA\voice_train.txt'
+path_uid = r'E:\python.file\structure data\JDATA\uid_train.txt'
+path_sms = r'E:\python.file\structure data\JDATA\sms_train.txt'
+path_wa = r'E:\python.file\structure data\JDATA\wa_train.txt'
+
+
+#create a spark shell to load .txt data
+sc = SparkContext()
+voice = sc.textFile(path_voice)
+uid = sc.textFile(path_uid)
+sms = sc.textFile(path_sms)
+wa = sc.textFile(path_wa)
+mode = lambda x: x.split('\t')
+voicedata = voice.map(mode)
+uiddata = uid.map(mode)
+smsdata = sms.map(mode)
+wadata = wa.map(mode)
+
+takeOrdered(3, key=lambda x: -x)
+
+joineddata = uiddata.join(voicedata)
+joineddata = joineddata.join(smsdata)
+
+
+#import sql module to convey data into dataframe
+#Dataframe is easy to sql
+from pyspark.sql.types import *
+from pyspark.sql import SparkSession
+from pyspark.sql import SQLContext
+spark = SparkSession.builder.appName("dataFrameApply").getOrCreate()
+#StructType:a list of column names,the type of each column will be inferred from data.
+schema = schema = StructType([
+    StructField("id",StringType(),True),
+    StructField("opp_num",StringType(),True),
+    StructField("opp_head",StringType(),True),
+    StructField("opp_len",StringType(),True),
+    StructField("start_time",StringType(),True),
+    StructField("end_time",StringType(),True),
+    StructField("call_type",StringType(),True),
+    StructField("in_out",StringType(),True)
+])
+#create a dataframe based on pyspark
+#register this df,so sql can be done on it(named voice)
+voiceDF = spark.createDataFrame(voicedata,schema)
+voiceDF.registerTempTable("voice")
+
+#Variable Derivation(VOICE)
+#def Derivation_call_type():
+call_type = {'LOCAL':1,'IN_PROVINCE':2,'BY_PROVINCE':3,'GAT':4,'INTERNATIONAL':5}
+for type_ in call_type:
+    sql_call = r" SELECT id,COUNT(id) FROM voice WHERE call_type= '{}' GROUP BY id".format(str(call_type[type_]))
+    exec('CALL_{} = spark.sql("{}")'.format(type_,sql_call))
+CALL_CNT = spark.sql(" SELECT id,COUNT(id) FROM voice GROUP BY id")
+
+
+
+
+
+
+
+
+
